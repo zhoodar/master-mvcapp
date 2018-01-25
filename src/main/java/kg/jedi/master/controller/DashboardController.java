696295941415629e60582ae5e3ca979764d79017
Zhoodar <zhoodar.abdikadyr@zensoft.io>
2018-01-25 19:58:06 +0600

    feat: implement review for master feature

--- src/main/java/kg/jedi/master/controller/DashboardController.java
+++ src/main/java/kg/jedi/master/controller/DashboardController.java
@@ -2,11 +2,14 @@ package kg.jedi.master.controller;
 
 import kg.jedi.master.common.PathNames;
 import kg.jedi.master.domain.Master;
+import kg.jedi.master.domain.Review;
 import kg.jedi.master.repository.MasterRepository;
+import kg.jedi.master.repository.ReviewRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Jedi on 1/23/18.
@@ -15,22 +18,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class DashboardController {
 
     private final MasterRepository masterRepository;
+    private final ReviewRepository reviewRepository;
 
-    public DashboardController(MasterRepository masterRepository) {
+    public DashboardController(MasterRepository masterRepository, ReviewRepository reviewRepository) {
         this.masterRepository = masterRepository;
+        this.reviewRepository = reviewRepository;
     }
 
-    @GetMapping(value = {"/"})
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("masters", masterRepository.findAll());
         return PathNames.INDEX_PAGE;
     }
 
-    @GetMapping(value = {"/masters/{id:[\\d]+}/appointment"})
-    public String appointment(@PathVariable("id") Long masterId, Model model) {
-        Master one = masterRepository.findOne(masterId);
-        model.addAttribute("master", one);
+    @GetMapping("/masters/{id:[\\d]+}/review/new")
+    public String processReview(@PathVariable("id") Long id, Model model) {
+        Master master = masterRepository.findOne(id);
+        Review review = new Review();
+        review.setMaster(master);
+        model.addAttribute(review);
+        return PathNames.REVIEW_PAGE;
+    }
 
-        return PathNames.APPOINTMENT_PAGE;
+    @PostMapping("/masters/{id:[\\d]+}/review/new")
+    public String processReview(@PathVariable("id") Long id, Review review) {
+        review.setId(null);
+        reviewRepository.save(review);
+
+        return "redirect:/masters/"+id;
     }
 }
